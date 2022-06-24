e2ce3d74e6cc736f0b4b0418712aaf166d860742
Choxy5 <kristijan.blazevic000@gmail.com>
2022-06-24 19:01:46 +0200

    Homepage Lists

--- client/src/pages/Home/Index.jsx
+++ client/src/pages/Home/Index.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
+import Featured from '../../components/Featured/Index';
+import FeaturedProperties from '../../components/FeaturedProperties/Index';
 import Header from '../../components/Header/Index';
 import Navbar from '../../components/Navbar/Index';
+import PropertyList from '../../components/PropertyList/Index';
 import './style.css';
 
 function Home() {
@@ -8,6 +11,13 @@ function Home() {
     <div>
       <Navbar />
       <Header />
+      <div className="homeContainer">
+        <Featured />
+        <h1 className="homeTitle">Browse by property type</h1>
+        <PropertyList />
+        <h1 className="homeTitle">Homes quests love</h1>
+        <FeaturedProperties />
+      </div>
     </div>
   );
 }
