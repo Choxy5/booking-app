3e8d44d9a71e94e674acd7043bb32a975315b0b8
Choxy5 <kristijan.blazevic000@gmail.com>
2022-06-24 18:17:47 +0200

    Advanced Search Bar with a Calendar

--- client/src/pages/List/Index.jsx
+++ client/src/pages/List/Index.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import Header from '../../components/Header/Index';
+import Navbar from '../../components/Navbar/Index';
 
 function List() {
-  return <div>List</div>;
+  return (
+    <div>
+      <Navbar />
+      <Header type="list" />
+    </div>
+  );
 }
 
 export default List;
